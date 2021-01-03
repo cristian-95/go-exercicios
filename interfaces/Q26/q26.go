036562e661f07f476e34f16612c301a75551707e
cristian <nem.cristianrc@gmail.com>
2021-01-03 14:57:44 -0300

    modificaçao de q26.go e deleçao de README.md

--- interfaces/Q26/q26.go
+++ interfaces/Q26/q26.go
@@ -1,3 +1,10 @@
+/*Q26. (2) Interfaces and max()
+
+In exercise Q13 we created a max function that works on a slice of integers. The
+question now is to create a program that shows the maximum number and that
+works for both integers and floats. Try to make your program as generic as possible,
+although that is quite difficult in this case.
+*/
 package main
 
 import (
