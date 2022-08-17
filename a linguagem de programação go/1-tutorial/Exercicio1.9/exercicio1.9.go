8e7454e27de215e92d404f4e50db0e773db20973
cristian-95 <nem.cristianrc@gmail.com>
2022-08-17 18:38:56 -0300

    exercicio 1.9

---
+++ a linguagem de programação go/1-tutorial/Exercicio1.9/exercicio1.9.go
@@ -0,0 +1,32 @@
+// Exercicio 1.9 - Modifique fetch para exibir também o código
+// de status HTTP encontrado em resp.Status.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		stats := resp.Status
+		fmt.Printf("Request to %s\nStatus: %s\n--------------------------------------------------------------------\nContent:\n\n", url, stats)
+		fmt.Printf("%s", b)
+	}
+
+}
